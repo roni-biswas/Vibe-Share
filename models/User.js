0bcd5c1c256a1529cc6be7f573f242ad6f7bdccb
Roni Biswas <ronibiswas068@gmail.com>
2026-03-02 19:45:25 +0600

    create user schema and also user model

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["public", "user", "admin"], // allowed values for the role field
+      default: "user", // default role for new users
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("User", UserSchema);

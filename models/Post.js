7c783c6a3ca45621a758de6e8c9bc96ffa1aa6fd
Roni Biswas <ronibiswas068@gmail.com>
2026-03-05 11:53:51 +0600

    create post schema and also post model

---
+++ models/Post.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: [100, "Title cannot be more then 100 characters"],
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      url: {
+        type: String,
+        required: true,
+      },
+      public_id: {
+        type: String,
+        required: true,
+      },
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ["Technology", "Lifestyle", "Coding", "Education"],
+      default: "Technology",
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Post", PostSchema);
